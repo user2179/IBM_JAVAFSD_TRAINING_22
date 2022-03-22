df76318926dc2bccd38152897e4abcc4cfbefa17
SURYA PRAKASH GADDALA <32205007+user2179@users.noreply.github.com>
2022-03-22 15:00:23 +0530

    Add files via upload

---
+++ Typescript/test.js
@@ -0,0 +1,8 @@
+console.log("this is not a drill");
+var a = 10;
+console.log(a * a);
+function subr(a, b, c) {
+    if (b === void 0) { b = 1; }
+    return a - b;
+}
+console.log(subr(6, 8));
---
+++ Typescript/test.ts
@@ -0,0 +1,9 @@
+console.log("this is not a drill");
+var a = 10;
+console.log(a*a);
+
+function subr(a,b=1,c?){
+	return a-b;
+}
+
+console.log(subr(6,8));
